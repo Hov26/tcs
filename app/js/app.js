f1212acba6630ae54ff231a9da05dd761e48c71c
Marish04 <94753536+Marish04@users.noreply.github.com>
2022-03-23 10:40:18 -0700

    correct section header

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,28 @@
-$(document).ready(function () {});
+// function openMenu() {
+//     $(".dropdown-profile").addClass("opened");
+//   }
+
+//   function closeMenu() {
+//     $(".dropdown-profile").removeClass("opened");
+//   }
+
+//   $(".dropdown-menu").click(function () {
+//     if ($('.dropdown-profile').hasClass('opened')) {
+//       closeMenu()
+//     } else {
+//       openMenu()
+//     }
+//   });
+
+const userBtn = document.querySelector(".dropdown-menu");
+const dropDown = document.querySelector(".dropdown-profile");
+let menuOpen = false;
+userBtn.addEventListener("click", () => {
+    if(!menuOpen) {
+        dropDown.classList.add("open");
+        menuOpen = true;
+    } else {
+        dropDown.classList.remove("open");
+        menuOpen = false;
+    }
+});
